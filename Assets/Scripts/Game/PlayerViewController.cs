67eaa56a922c8aeb1c1b19951e9dc50da0272e64
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-21 20:01:05 +0300

    Fix init error. Use DeltaTime in rotation.

--- Assets/Scripts/Game/PlayerViewController.cs
+++ Assets/Scripts/Game/PlayerViewController.cs
@@ -4,8 +4,8 @@ public class PlayerViewController : MonoBehaviour
 {
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
-    private float sensitivityX = 5f;
-    private float sensitivityY = 5f;
+    private float sensitivityX = 100.0f;
+    private float sensitivityY = 100.0f;
 
     private float minimumX = 0f;
     private float maximumX = 360f;
@@ -19,6 +19,7 @@ public class PlayerViewController : MonoBehaviour
 
     public void Init()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.freezeRotation = true;
     }
 
@@ -26,20 +27,20 @@ public class PlayerViewController : MonoBehaviour
     {
         if (axes == RotationAxes.MouseXAndY)
         {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX * dt;
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY * dt;
             rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
             transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
         }
         else if (axes == RotationAxes.MouseX)
         {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX * dt, 0);
         }
         else
         {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY * dt;
             rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
             transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
