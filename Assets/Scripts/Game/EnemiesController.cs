5555f3bd320c474b1b38b50ff4a5d182fef27683
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-21 23:25:45 +0300

    Prepare EnemiesController & ClickLighter event.

---
+++ Assets/Scripts/Game/EnemiesController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemiesController : MonoBehaviour
+{
+    public delegate void ClickLighterAction(Vector3 position);
+    public static event ClickLighterAction OnClickLighterAction;
+
+    public void ClickLight(Vector3 position)
+    {
+        if (OnClickLighterAction != null)
+            OnClickLighterAction(position);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Update click!");
+            ClickLight(gameObject.transform.position);
+        }
+    }
+}
