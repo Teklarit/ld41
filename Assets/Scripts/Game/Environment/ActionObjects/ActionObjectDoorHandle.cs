0402e16a886deadaf394682c11ae4256ca6e6ebb
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-22 21:56:00 +0300

    Door opening & handle shaking is work.

---
+++ Assets/Scripts/Game/Environment/ActionObjects/ActionObjectDoorHandle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ActionObjectDoorHandle : ActionObject
+{
+    [SerializeField] private DoorHandleController _doorHandleController;
+
+    public override void Activate(ActionObjectActivator.ET_ACTIVATE_TYPE activateType, float time = 0)
+    {
+        base.Activate(activateType, time);
+
+        _doorHandleController.SetShakeSeconds(10);
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+
+        //GetComponent<DoorHandleController>().SetShakeSeconds(0);
+    }
+
+}
