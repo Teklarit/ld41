49ea2c65f79e24c95937c87799389db52d81bc8f
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-22 23:20:38 +0300

    Start soundController.

---
+++ Assets/Scripts/Game/SoundController.cs
@@ -0,0 +1,9 @@
+﻿using TheHeartbeat.Utilities;
+using UnityEngine;
+
+public class SoundController : SingletonBehaviour<SoundController>
+{
+    [SerializeField] private AudioSource _audioSource;
+
+
+}
