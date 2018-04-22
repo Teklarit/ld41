88c5b35e52ef6502e21035c096b73bcab5dfdca4
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-22 17:26:40 +0300

    Doors works with trigger! Add inverse activation trigger.

---
+++ Assets/Scripts/Game/Environment/ActionObjects/ActionObjectDoor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ActionObjectDoor : ActionObject
+{
+    public override void Activate(ActionObjectActivator.ET_ACTIVATE_TYPE activateType, float time = 0)
+    {
+        base.Activate(activateType, time);
+
+        GetComponent<DoorController>().SetDoorTarget(true, DoorController.ET_DOOR_SPEED.SLOW);
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+
+        GetComponent<DoorController>().SetDoorTarget(false, DoorController.ET_DOOR_SPEED.FAST);
+    }
+}
