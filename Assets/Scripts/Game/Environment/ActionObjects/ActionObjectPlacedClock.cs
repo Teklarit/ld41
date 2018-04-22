f2f13c58a3e4b9c7d394ca89cbe62443d1128b3b
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-22 21:00:45 +0300

    Clock & radio prefabs.

---
+++ Assets/Scripts/Game/Environment/ActionObjects/ActionObjectPlacedClock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ActionObjectPlacedClock : ActionObject
+{
+    public override void Activate(ActionObjectActivator.ET_ACTIVATE_TYPE activateType, float time = 0)
+    {
+        base.Activate(activateType, time);
+
+
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+
+
+    }
+}
---
+++ Assets/Scripts/Game/Environment/ActionObjects/ActionObjectRadio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ActionObjectRadio : ActionObject
+{
+    public override void Activate(ActionObjectActivator.ET_ACTIVATE_TYPE activateType, float time = 0)
+    {
+        base.Activate(activateType, time);
+
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+
+
+    }
+}
