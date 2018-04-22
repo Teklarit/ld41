ab5f1d382d2baa96547ce64d1605464f31451599
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-22 14:10:44 +0300

    Add door. Opening. Handle shake.

---
+++ Assets/Scripts/Game/Environment/DoorHandleController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandleController : MonoBehaviour
+{
+    [SerializeField] private Transform _doorHandle;
+    [SerializeField] private AnimationCurve _animCurve;
+    [Space]
+    [SerializeField] private float _minWaveTime = 1.0f;
+    [SerializeField] private float _maxWaveTime = 2.0f;
+    [Space]
+    [SerializeField] private float _maxShakeAngle = 8.0f;
+    [Space]
+    [SerializeField] private bool _shakeX = false;
+    [SerializeField] private bool _shakeY = true;
+    [SerializeField] private bool _shakeZ = true;
+
+    private float _needShakeTime = 0.0f;
+    private Vector3 _startLocalEulerAngles;
+    private float _waveTime = 0.0f;
+    private float _waveCurrent = -1.0f;
+    private Vector3 _shakeLocalEuler = new Vector3(15, 15, 15);
+
+    public void SetShakeSeconds(float t)
+    {
+        _needShakeTime = t;
+    }
+
+    private void Start()
+    {
+        _startLocalEulerAngles = _doorHandle.localEulerAngles;
+    }
+
+    private void Update()
+    {
+        _needShakeTime -= Time.deltaTime;
+        _waveCurrent -= Time.deltaTime;
+
+        if (_needShakeTime >= 0.0f)
+        {
+            if (_waveCurrent <= 0) // new wave
+            {
+                _waveTime = Random.Range(_minWaveTime, _maxWaveTime);
+                _waveCurrent = _waveTime;
+
+                float axisX = _shakeX ? Random.Range(-_maxShakeAngle, _maxShakeAngle) : 0;
+                float axisY = _shakeY ? Random.Range(-_maxShakeAngle, _maxShakeAngle) : 0;
+                float axisZ = _shakeZ ? Random.Range(-_maxShakeAngle, _maxShakeAngle) : 0;
+                _shakeLocalEuler = new Vector3(
+                    _startLocalEulerAngles.x + axisX
+                    , _startLocalEulerAngles.y + axisY
+                    , _startLocalEulerAngles.z + axisZ);
+            }
+        }
+
+        if (_waveCurrent >= 0)
+        {
+            float ratio = Mathf.Clamp01(1.0f - (_waveCurrent / _waveTime));
+            Vector3 newLocalEulerAngles = Vector3.Lerp(_startLocalEulerAngles, _shakeLocalEuler, _animCurve.Evaluate(ratio));
+            _doorHandle.localEulerAngles = newLocalEulerAngles;
+        }
+    }
+}
