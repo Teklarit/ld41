593bee97d34fedd75cbba8e06b75e32f97d62bdc
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-22 21:28:56 +0300

    ActivatorTrigger use mask.

--- Assets/Scripts/Game/Environment/Activator/ActionObjectActivatorTrigger.cs
+++ Assets/Scripts/Game/Environment/Activator/ActionObjectActivatorTrigger.cs
@@ -4,10 +4,17 @@ public class ActionObjectActivatorTrigger : ActionObjectActivator
 {
     [Space]
     [SerializeField] private Collider _collider;
+    [Space]
+    [SerializeField] private LayerMask _layerMaskFilter;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("ENTER Trigger");
-        TryActivate();
+        //Debug.Log("TriggerEnter");
+        if ((_layerMaskFilter.value & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+        {
+            //Debug.Log("Trigger good mask!");
+            TryActivate();
+        }
     }
 }
