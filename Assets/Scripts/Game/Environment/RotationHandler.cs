6490bddfc2ea0937d6d8e83f95928b6fdf971d0d
Mykyta <smaganik@gmail.com>
2018-04-24 02:31:56 +0300

    Main scene improved.

--- Assets/Scripts/Game/Environment/RotationHandler.cs
+++ Assets/Scripts/Game/Environment/RotationHandler.cs
@@ -4,13 +4,22 @@ public class RotationHandler : MonoBehaviour
 {
     [SerializeField] private float _angularSpeed;
 
+    private Quaternion _initialRotation;
+    private Vector3 _initialUp;
+    private float _angle;
+
     private void Awake()
     {
-        transform.rotation *= Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.up);
+        _angle = Random.Range(0f, 360f);
+        _initialUp = transform.up;
+        _initialRotation = transform.localRotation;
+        transform.localRotation = Quaternion.AngleAxis(_angle, _initialUp) * _initialRotation;
     }
 
     private void Update()
     {
-        transform.rotation *= Quaternion.AngleAxis(_angularSpeed * Time.deltaTime, Vector3.up);
+        _angle += _angularSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.AngleAxis(_angle, _initialUp) * _initialRotation;
+        //transform.rotation *= Quaternion.AngleAxis(_angularSpeed * Time.deltaTime, _initialUp);
     }
 }
