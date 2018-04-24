e9288e38febe1f65d3ed5cbc8e0cb833a3729b06
TEKLARITPC\Teklarit <Teklarit@yandex.ru>
2018-04-24 03:25:33 +0300

    Fix script.

--- Assets/Scripts/Game/Environment/ActionObjects/ActionObjectCrowlingMonster.cs
+++ Assets/Scripts/Game/Environment/ActionObjects/ActionObjectCrowlingMonster.cs
@@ -4,7 +4,7 @@ public class ActionObjectCrowlingMonster : ActionObject
 {
     [SerializeField] private float _timeCrowle = 6.0f;
     [SerializeField] private float _moveSpeed = 1.0f;
-    private bool _needMove = true;//false;
+    private bool _needMove = false;
 
     public override void Activate(ActionObjectActivator.ET_ACTIVATE_TYPE activateType, float time = 0)
     {
